bdc0bde069acf0d58c3ae5230b7884fc531f0d81
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2022-09-24 22:12:35 +0530

    BookReport Service and controller

---
+++ BookReportTest/BookReportTest/BookReportTest/Controllers/BookReportController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookReportTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookReportController : ControllerBase
+    {
+        public BookReportController()
+        {
+        }
+
+        [HttpGet]
+        [Route("{reportName}/last")]
+        public ActionResult GetLast()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("{reportName}/{lang}")]
+        public ActionResult Create()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{reporId}/{pageNumber}/data")]
+        public ActionResult GetPage()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{reportId}")]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
